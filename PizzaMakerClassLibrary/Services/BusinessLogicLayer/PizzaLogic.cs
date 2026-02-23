9b312c478a5f483d11869a935df479a7d378fae2
MosesMnz <39951731+MosesMNZ@users.noreply.github.com>
2026-02-23 03:00:23 -0700

    : Set up the PizzaLogic class and add logic to add a pizza to the order.

--- PizzaMakerClassLibrary/Services/BusinessLogicLayer/PizzaLogic.cs
+++ PizzaMakerClassLibrary/Services/BusinessLogicLayer/PizzaLogic.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * Moses Muamba-Nzambi
+ * CST – 250
+ * Feb 22, 2026
+ * Pizza Maker
+ * Activity 4
+ */
+
+using PizzaMaker.Models;
+using PizzaMakerClassLibrary.Services.DataAccessLayer;
 
 namespace PizzaMakerClassLibrary.Services.BusinessLogicLayer
 {
-    internal class PizzaLogic
+    public class PizzaLogic
     {
+        // Declare class level variables
+        private PizzaDAO _pizzaDAO;
+
+        /// <summary>
+        /// Default constructor for PizzaLogic
+        /// </summary>
+        public PizzaLogic()
+        {
+            // Initialize the pizza DAO object
+            _pizzaDAO = new PizzaDAO();
+        }
+
+        /// <summary>
+        /// Add a new pizza to the current order
+        /// </summary>
+        /// <param name="newPizza"></param>
+        /// <returns></returns>
+        public (bool isValidPizza, int pizzasInOrder) AddPizzaToOrder(PizzaModel newPizza)
+        {
+            // Declare and initialize
+            int pizzas = -1;
+
+            // Call the DAO AddPizzaToOrder
+            pizzas = _pizzaDAO.AddPizzaToOrder(newPizza);
+
+            // Return the pizzas variable
+            return (true, pizzas);
+        }
     }
 }
